67506bd8c6f97b6fd993613c00a4008f17fb585d
marufnahid <maruf155714@outlook.com>
2023-03-26 14:35:43 +0600

    Getter and Setter from main branch

---
+++ app/src/main/java/com/example/myloginapp/Test.java
@@ -0,0 +1,28 @@
+package com.example.myloginapp;
+
+public class Test {
+
+    String name;
+    int roll;
+
+    public Test(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
